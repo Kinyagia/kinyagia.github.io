8c35070c9c29136fc6fa7b10d3244da12a460aa0
Morris <mburumorris@gmail.com>
2018-12-27 19:09:35 +0100

    Updates

---
+++ precache-manifest.d1dedb21685a74254233b0c493890c3c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a283f988f7e2639a5a05ae488e316434",
+    "url": "/static/media/mail.a283f988.svg"
+  },
+  {
+    "revision": "269a91d08a9e42a181a95d26be2a5523",
+    "url": "/static/media/linkedIn.269a91d0.svg"
+  },
+  {
+    "revision": "585282f5b3bdd166555dbc8d201b7267",
+    "url": "/static/media/github.585282f5.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cf2e8af3581f77959984",
+    "url": "/static/js/main.cf2e8af3.chunk.js"
+  },
+  {
+    "revision": "25aee797ba5d66677279",
+    "url": "/static/js/1.25aee797.chunk.js"
+  },
+  {
+    "revision": "cf2e8af3581f77959984",
+    "url": "/static/css/main.823912dc.chunk.css"
+  },
+  {
+    "revision": "c458614182f4812f1d2156f4439ff945",
+    "url": "/index.html"
+  }
+];
