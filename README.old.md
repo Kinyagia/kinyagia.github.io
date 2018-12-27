25e30c3e229b8cc530b57eaa424417e90e0e5462
Morris <mburumorris@gmail.com>
2018-12-27 16:40:00 +0100

    added icons

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# kinyagia.github.io
-Personal Website
--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import mail from './mail.svg';
+import github from './github.svg';
+import linkedIn from './linkedIn.svg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
 	  <nav> 
 	    <ul> 
               <li><a href="#">Home  </a></li> 
-              <li><a href="#">Projects  </a></li> 
-              <li><a href="#">About </a></li>
-              <li><a href="#"> Contacts  </a></li>
-          </ul>
+              <li><a href="#projects">Projects  </a></li> 
+              <li><a href="#about">About </a></li>
+              <li><a href="#contacts"> Contacts  </a></li>
+            </ul>
           </nav>
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Morris Mburu<br/> 
+            I am a Front-end dev currently living in Mainz, <br/>
+            where I go to School. 
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      <div className="body">
-      </div>	
-      </div>
+          <p id="projects">
+           Cool projects,<br/>
+           I am currently working on.
+          </p>
+          <p id="about">
+           I&#39;m twenty   <br/>
+           My hobbys are Cinema,listening to old timey tunes,
+           and Podcasts.
+          </p>
+          <footer className="contacts" id="contacts">
+            <ul>
+             <li><img src={mail} className="App-logo" /></li> 
+              <li><img src={github} className="App-logo" /></li>  
+              <li><img src={linkedIn} className="App-logo" /></li> 
+            </ul>
+          </footer>
+          </div>
     );
   }
 }
